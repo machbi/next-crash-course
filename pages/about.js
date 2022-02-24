0ae1e63a6ee00f0c66bf4b7972a9d294206c7a1f
Mark Bijl <mark_bijl@Hotmail.com>
2022-02-24 09:17:52 +0100

    Test

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+const about = () => {
+    return (
+      <div>  
+        <Head>
+        <title>About</title>
+        <meta name='keywords' content='web development, programming' />
+        <h1>About</h1>
+        </Head>
+       </div>
+    )
+}
+
+export default about
